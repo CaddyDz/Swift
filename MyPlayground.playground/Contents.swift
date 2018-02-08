987087943f6c3c9fffaaad89db46c1e2638e9fc1
Caddy Dz <tbitw31@gmail.com>
2018-02-08 13:12:51 +0100

    Add missing comments to intro
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,18 @@
+// Gives access to Cocoa Framework API
 import Cocoa
+
+// Other random functions I need
 import Darwin
 
+// var declares this string variable
+// Statements don't require ;
 var hello = "Hello "
-var world = "World"
+
+// You can define it is a string
+var world: String = "World"
+
+// Combine strings
 var msg = hello + world
+
+// You can place variables in print
 print("Hello \(world)")
