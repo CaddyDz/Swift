336bae87daaeb1256c0fd4617821fa4315ec90f1
Caddy Dz <tbitw31@gmail.com>
2018-02-08 17:24:49 +0100

    Ternary Operator
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,32 +4,9 @@ import Cocoa
 // Other random functions I need
 import Darwin
 
-// ----- CONDITIONALS -----
-
-// ----- IF / ELSE -----
-
-// Comparison Operators : > < >= <= == !=
-// === : Checks if pointing to same reference
-// !== : Checks if don't point at same reference
-
 var age: Int = 8
 
-if age < 5 {
-    print("Go to Preschool")
-} else if age == 5 {
-    print("Go to Kindergarten")
-} else if (age > 5) && (age <= 18) {
-    var grade: Int = age - 5
-    print("Go to Grade \(grade)")
-} else {
-    print("Go to College")
-}
+// ----- TERNARY OPERATOR -----
+// Assigns 1 of 2 values depending on condition
 
-// Or logical operator
-var income: Double = 12000
-var gpa: Double = 3.7
-
-print("Get Grant : \((income < 15000) || (gpa >= 3.8))")
-
-// Not operator
-print("Not True : \(!true)")
+var canDrive: Bool = age >= 16 ? true : false
