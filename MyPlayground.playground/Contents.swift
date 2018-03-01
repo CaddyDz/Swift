30cb94f5b6f42d35a89e40b9c9fe928f406b7ad6
Caddy Dz <tbitw31@gmail.com>
2018-03-01 13:54:22 +0100

    Math
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,22 +4,34 @@ import Cocoa
 // Other random functions I need
 import Darwin
 
-// ----- SWITCH -----
-// Matches a limited number of values and doesn't all
-// through after a match
+// ----- MATH -----
 
-// You can also match ranges
-let testScore: Int = 89
+print("5 + 4 = \(5 + 4)")
+print("5 - 4 = \(5 - 4)")
+print("5 * 4 = \(5 * 4)")
+print("5.0 / 4.0 = \(5.0 / 4.0)")
+print("5 % 4 = \(5 % 4)")
 
-switch testScore {
-    case 93...100:
-        print("You got an A")
-    case 85...92:
-        print("You got a B")
-    case 77...84:
-        print("You got a C")
-    case 69...76:
-        print("You got a D")
-    default:
-        print("You got an F")
-}
+// ----- ORDER OF OPERATIONS -----
+print("4 + 5 * 10 = \(4 + 5 * 10)")
+print("(4 + 5) * 10 = \((4 + 5) * 10)")
+
+// Shortcuts
+var randNum: Int = 5
+randNum += 1
+randNum -= 1
+
+// You can't do randNum++ in Swift 3
+
+// Math Functions
+print("abs(-5) = \(abs(-5))")
+print("floor(5.5) = \(floor(5.5))")
+print("ceil(5.4) = \(ceil(5.4))")
+print("max(5,4) = \(max(5,4))")
+print("min(5,4) = \(min(5,4))")
+print("pow(5,2) = \(pow(5,2))")
+print("sqrt(25) = \(sqrt(25))")
+print("log(2.71828) = \(log(2.71828))")
+
+// There is also sin, cos, tan, asin, acos, atan
+// sinh, cosh, tanh
