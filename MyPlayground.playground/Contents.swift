b7ee11a779c8ad6227ff5d172fbe28ed3440450b
Caddy Dz <tbitw31@gmail.com>
2018-03-04 20:55:49 +0100

    Strings
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,34 +4,44 @@ import Cocoa
 // Other random functions I need
 import Darwin
 
-// ----- MATH -----
+// ----- STRINGS -----
+// A string of characters that are passed by value
+// Escape Characters : \\ \t \n \" \'
+var randStr = "This is a random string"
+var randStr2 = " and here is another"
 
-print("5 + 4 = \(5 + 4)")
-print("5 - 4 = \(5 - 4)")
-print("5 * 4 = \(5 * 4)")
-print("5.0 / 4.0 = \(5.0 / 4.0)")
-print("5 % 4 = \(5 % 4)")
+// Join strings
+var randStr3 = randStr + randStr2
 
-// ----- ORDER OF OPERATIONS -----
-print("4 + 5 * 10 = \(4 + 5 * 10)")
-print("(4 + 5) * 10 = \((4 + 5) * 10)")
+// String length
+print("Length : \(randStr3.characters.count)")
 
-// Shortcuts
-var randNum: Int = 5
-randNum += 1
-randNum -= 1
+// Get the first character
+print("First : \(randStr3[randStr3.startIndex])")
 
-// You can't do randNum++ in Swift 3
+// Get the 5th character
+let index5 = randStr3.index(randStr3.startIndex, offsetBy: 5)
+print("5th : \(randStr3[index5])")
 
-// Math Functions
-print("abs(-5) = \(abs(-5))")
-print("floor(5.5) = \(floor(5.5))")
-print("ceil(5.4) = \(ceil(5.4))")
-print("max(5,4) = \(max(5,4))")
-print("min(5,4) = \(min(5,4))")
-print("pow(5,2) = \(pow(5,2))")
-print("sqrt(25) = \(sqrt(25))")
-print("log(2.71828) = \(log(2.71828))")
+// Check if string is empty
+print("Empty : \(randStr.isEmpty)")
 
-// There is also sin, cos, tan, asin, acos, atan
-// sinh, cosh, tanh
+// Insert a character at an index
+randStr2.insert("A", at: randStr2.startIndex)
+
+// Insert a string at an index
+randStr2.insert(contentsOf: " string ".characters, at: randStr2.index(randStr2.startIndex, offsetBy: 2))
+print(randStr2)
+
+// Get a substring
+let startIndex = randStr2.index(randStr2.startIndex, offsetBy: 2)
+let endIndex = randStr2.index(randStr2.startIndex, offsetBy: 9)
+let stringRange = startIndex ..< endIndex
+let subStr = randStr2.substring(with: stringRange)
+
+// Replace a string
+// Check if there is a match
+if let hereMatch = randStr2.range(of: "here"){
+    randStr2.replaceSubrange(hereMatch, with: "there")
+}
+print(randStr2)
