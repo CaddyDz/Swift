9f7bff7804d10a174ddf891319a25776c812e8bd
Caddy Dz <tbitw31@gmail.com>
2018-03-06 21:22:17 +0100

    Looping
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,51 +4,56 @@ import Cocoa
 // Other random functions I need
 import Darwin
 
-// ----- ARRAYS -----
-// Stores values of the same type
+// ----- LOOPING -----
 
-// Make empty array
-var array1 = [Int]()
+// ----- FOR LOOP ------
 
-// Check if array is empty
-print("Empty \(array1.isEmpty)")
+var array4 = [1, 2, 3]
 
-// Add value to array
-array1.append(5)
-
-// Add another item
-array1 += [7, 9]
-
-// Get array item
-print("Index 1 : \(array1[1])")
-
-// Change value at index
-array1[0] = 4
-
-// Insert at an index
-array1.insert(10, at: 3)
-
-// Remove item
-array1.remove(at: 3)
-
-// Change multiple values
-array1[0...2] = [1,2,3]
-
-// Length of array
-print("Length : \(array1.count)")
-
-// Fill array with a value
-var array2 = Array(repeating: 0, count: 5)
-
-// Combine arrays
-var array3 = array1 + array2
-
-// Iterate through an array
-for item in array3 {
+// Iterate through array
+for item in array4 {
     print(item)
 }
 
-// Get index and value
-for (index, value) in array3.enumerated() {
-    print("\(index) : \(value)")
+// Iterate with a range
+for i in 1...5 {
+    print(i)
 }
+
+// ----- WHERE -----
+// Print only evens
+for i in 1...10 where i % 2 == 0 {
+    print("Even : \(i)")
+}
+
+// ----- WHILE LOOP -----
+var i: Int = 1
+while i < 10 {
+    if i % 2 == 0 {
+        i += 1
+        // Continue jumps back to the beginning of the loop
+        continue
+    }
+    if i == 7 {
+        
+        // Break ends the loop
+        break
+    }
+    
+    print(i)
+    i += 1
+}
+
+// ----- REPEAT WHILE -----
+
+// Generate a random number
+let magicNum: UInt32 = arc4random_uniform(10)
+var guess: UInt32 = 0
+
+repeat {
+    print("Guess : \(guess)")
+    guess += 1
+} while (magicNum != guess)
+
+print("Magic Number was \(magicNum)")
+
