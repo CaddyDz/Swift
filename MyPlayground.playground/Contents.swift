e2d2bb66b5de61288b34c0deacab8821cece6d61
Caddy Dz <tbitw31@gmail.com>
2018-03-08 21:08:45 +0100

    Classes
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,22 +4,39 @@ import Cocoa
 // Other random functions I need
 import Darwin
 
-// ----- STRUCTS -----
-// Structs group related data together
+// ----- CLASSES -----
 
-struct Rectangle {
-    var height = 0.0
-    var length = 0.0
+class Animal {
+    var name: String = "No Name"
+    var height: Double = 0.0
+    var weight: Double = 0.0
+    var sound: String = "No Sound"
     
-    // You can include functions
-    func area() -> Double {
-        let area = height * length
-        return area
+    // Assigns default values when an object is created
+    // You can have many inits with different attributes
+    
+    // self is used to refer to attributes of the object that called for this method to execute
+    init(name: String, height: Double, weight: Double, sound: String) {
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.sound = sound
+    }
+    
+    func getInfo() {
+        print("\(self.name) is \(self.height) cms tall and weighs \(self.weight) kgs and likes to say \(self.sound)")
+    }
+    
+    // You can create overloaded methods if you change the attributes
+    func getSum(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    func getSum(num1: Double, num2: Double) -> Double {
+        return num1 + num2
     }
 }
 
-// Create a Rectangle
-let myRect = Rectangle(height: 10.0, length: 5.0)
+var rover = Animal(name: "Rover", height: 38, weight: 12.7, sound: "Ruff")
 
-print("Area : \(myRect.height) * \(myRect.length) = \(myRect.area())")
-
+rover.getInfo()
