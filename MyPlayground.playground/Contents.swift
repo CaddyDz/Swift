8f501857f5fb73408711b5738a6bbcc1b18c4a29
Salim Djerbouh <github@caddydz.me>
2018-06-01 17:11:27 +0100

    Bronze Challenge
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,8 +5,11 @@ let numberOfStopLights: Int = 4
 var population: Int
 population = 5422
 
+var levelOfUnemployment: Double
+levelOfUnemployment = 28.98
+
 let townName: String = "Knowhere"
 
-let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stop lights"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stop lights, it has a level of unemployment of \(levelOfUnemployment)"
 
 print(townDescription)
