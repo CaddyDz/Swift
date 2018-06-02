198dcdd1a4077db6ee0ac758fd12e845a2f8bc81
Salim Djerbouh <github@caddydz.me>
2018-06-02 19:57:09 +0100

    Bronze challenge
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,19 @@
 import Cocoa
 
-var population: Int = 5422
+var population: Int = 100000
 var message: String
 var hasPostOffice: Bool = true
 
 if population < 10000 {
     message = "\(population) is a small town!"
 } else if population >= 10000 && population < 50000 {
-        message = "\(population) is a medium town!"
-    } else {
-     message = "\(population) is pretty big!"
-    }
+    message = "\(population) is a medium town!"
+} else if population >= 100000 {
+    message = "\(population) is really large"
+}
+else {
+    message = "\(population) is pretty big!"
+}
 print(message)
 
 if !hasPostOffice {
