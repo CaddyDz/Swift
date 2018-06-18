9c6339b0d939b12b5cea062c27221c7e6aeebdf2
Salim Djerbouh <github@caddydz.me>
2018-06-18 19:58:03 +0100

    Using a loop to append items from one array to another
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,16 @@
 import Cocoa
 
 var bucketList = ["Climb Mt. Everest"]
-bucketList.append("Fly hot air balloon to Fiji")
-bucketList.append("Watch the Lord of the Rings tri")
-bucketList.append("Go on a walkabout")
-bucketList.append("Scuba dive in the Great Blue Hole")
-bucketList.append("Find a triple rainbow")
+var newItems = [
+    "Fly hot air balloon to Fiji",
+    "Watch the Lord of the Rings trilogy in one day",
+    "Go on a walkabout",
+    "Scuba dive in the Great Blue Hole",
+    "Find a triple rainbow"
+    ]
+for item in newItems {
+    bucketList.append(item)
+}
 bucketList.remove(at: 2)
 print(bucketList.count)
 print(bucketList[0...2])
