6ba3e7cac87d62ec0e6361b2458635e011962ab0
Salim Djerbouh <github@caddydz.me>
2018-06-18 19:50:34 +0100

    So many ambitions
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,3 +2,7 @@ import Cocoa
 
 var bucketList = ["Climb Mt. Everest"]
 bucketList.append("Fly hot air balloon to Fiji")
+bucketList.append("Watch the Lord of the Rings tri")
+bucketList.append("Go on a walkabout")
+bucketList.append("Scuba dive in the Great Blue Hole")
+bucketList.append("Find a triple rainbow")
