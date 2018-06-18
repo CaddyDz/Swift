02ed46272f425e7b6e5877cb933bf30666716da8
Salim Djerbouh <github@caddydz.me>
2018-06-18 19:41:25 +0100

    Creating an array
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,18 +1,3 @@
-// Optional chaining
 import Cocoa
 
-var errorCodeString = "404"
-var errorDescription: String?
-if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
-    errorDescription = "\(errorCodeInteger + 200): resource was not found"
-}
-
-var upCaseErrorDescription = errorDescription?.uppercased()
-errorDescription
-
-upCaseErrorDescription?.append(" Please Try Again.")
-upCaseErrorDescription
-
-errorDescription = nil
-let description = errorDescription ?? "No error"
-
+var bucketList: Array<String>
