6617254d4fcfb93c081979d779476ec44868820f
Salim Djerbouh <github@caddydz.me>
2018-06-18 15:42:41 +0100

    Optional Chaining
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,11 @@
+// Optional chaining
 import Cocoa
 
-var errorCodeString: String! = nil
+var errorCodeString = "404"
+var errorDescription: String?
+if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+    errorDescription = "\(errorCodeInteger + 200): resource was not found"
+}
 
-let anotherErrorCodeString: String = errorCodeString
-let yetAnotherErrorCodeString = errorCodeString
+var upCaseErrorDescription = errorDescription?.uppercased()
+errorDescription
