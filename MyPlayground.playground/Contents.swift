61aa886a32706628a4e120220800652b54693199
Salim Djerbouh <github@caddydz.me>
2018-06-22 16:51:47 +0100

    Optional Return Types
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -52,3 +52,13 @@ func sortedEvenOddNumbers(_ numbers: [Int]) -> (evens: [Int], odds: [Int]) {
 let aBunchOfNumbers = [10,1,4,3,57,43,84,27,157,111]
 let theSortedNumbers = sortedEvenOddNumbers(aBunchOfNumbers)
 print("The even numbers are: \(theSortedNumbers.evens); the odd numbers are: \(theSortedNumbers.odds)")
+
+func grabMiddleName(fromFullName name: (String, String?, String)) -> String? {
+    return name.1
+}
+
+let middleName = grabMiddleName(fromFullName: ("Matt", nil, "Mathiaw"))
+
+if let theName = middleName {
+    print(theName)
+}
