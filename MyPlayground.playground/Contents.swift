bfde079c01f87b07e8c12a5348b05cd66f77d13f
Salim Djerbouh <github@caddydz.me>
2018-06-22 15:50:38 +0100

    Default parameter values
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -14,7 +14,8 @@ func printPersonalGreetings(to names: String...) {
 
 printPersonalGreetings(to: "Ales", "Chris", "Drew", "Pat")
 
-func divisionDescriptionFor(numerator: Double, denominator: Double) {
-    print("\(numerator) divided by \(denominator) equals \(numerator / denominator)")
+func divisionDescriptionFor(numerator: Double, denominator: Double, withPunctuation punctuation: String = ".") {
+    print("\(numerator) divided by \(denominator) equals \(numerator / denominator)\(punctuation)")
 }
 divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0, withPunctuation: "!")
