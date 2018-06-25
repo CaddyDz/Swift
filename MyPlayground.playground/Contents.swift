5f637dde71f2ac28e73f834582d9c05ac70fb437
Salim Djerbouh <github@caddydz.me>
2018-06-25 09:53:36 +0100

    Functions as Return Types
    Return to Knowhere
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,10 @@ import Cocoa
 let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
 let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
+
+func makeTownGrand() -> (Int, Int) -> Int {
+    func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int {
+        return lights + existingLights
+    }
+    return buildRoads
+}
