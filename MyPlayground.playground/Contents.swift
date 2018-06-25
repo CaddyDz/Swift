f73c5294858f70eb12bf70dc0a73a0d9cfb4dc3e
Salim Djerbouh <github@caddydz.me>
2018-06-25 13:10:55 +0100

    Defining an enumeration
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,56 +1,7 @@
 import Cocoa
 
-let vollunteerCounts = [1,3,40,32,2,53,77,13]
-
-let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
-
-func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int)? {
-    if condition(budget) {
-        func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int {
-            return lights + existingLights
-        }
-        return buildRoads
-    } else {
-        return nil
-    }
+enum TextAlignment {
+    case left
+    case right
+    case center
 }
-
-func evaluate(budget: Int) -> Bool {
-    return budget > 10000
-}
-var stopLights = 4
-if let newTownPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 10500, condition: evaluate) {
-    stopLights = newTownPlanByAddingLightsToExistingLights(4, stopLights)
-}
-print("Knowhere has \(stopLights) stoplights.")
-
-func makePopulationTracker(forInitialPopulation population: Int) -> (Int) -> Int{
-    var totalPopulation = population
-    func populationTracker(growth: Int) -> Int {
-        totalPopulation += growth
-        return totalPopulation
-    }
-    return populationTracker
-}
-
-var currentPopulation = 5422
-let growBy = makePopulationTracker(forInitialPopulation: currentPopulation)
-growBy(500)
-growBy(500)
-growBy(500)
-currentPopulation = growBy(500) // currentPopulation is now 7422
-let anotherGrowBy = growBy
-anotherGrowBy(500) // totalPopulation now equal to 7922
-var bigCityPopulation = 4_061_981
-let bigCityGrowBy = makePopulationTracker(forInitialPopulation: bigCityPopulation)
-bigCityPopulation = bigCityGrowBy(10_000)
-currentPopulation
-let precinctPopulations = [1244, 2021, 2157]
-let projectedPopulations = precinctPopulations.map {$0 * 2}
-projectedPopulations
-let bigProjections = projectedPopulations.filter { $0 > 4000 }
-bigProjections
-
-let totalProjection = projectedPopulations.reduce(0)
-    {$0 + $1}
-totalProjection
