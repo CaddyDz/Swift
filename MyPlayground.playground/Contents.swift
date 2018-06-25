daf0f16450f3ac47188d4c72d8880a463f2d69a3
Salim Djerbouh <github@caddydz.me>
2018-06-25 09:39:06 +0100

    Closures
    Sorting the array
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import Cocoa
 
 let vollunteerCounts = [1,3,40,32,2,53,77,13]
+
+func sortedAscending(_ i: Int, _ j: Int) -> Bool {
+    return i < j
+}
+
+let volunteerSorted = vollunteerCounts.sorted(by: sortedAscending)
