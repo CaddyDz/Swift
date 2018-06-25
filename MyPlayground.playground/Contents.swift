51be8e97f039a9c2e321347060a7198569f23a17
Salim Djerbouh <github@caddydz.me>
2018-06-25 09:46:41 +0100

    Closure Expression Syntax
    Using shorthand syntax for arguments
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,4 @@ import Cocoa
 
 let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
-let volunteersSorted = vollunteerCounts.sorted(by: {i,j in i < j})
+let volunteersSorted = vollunteerCounts.sorted(by: {$0 < $1})
