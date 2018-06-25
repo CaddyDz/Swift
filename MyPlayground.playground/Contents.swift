e691cf8eb9577533376019ca7a735c935c257b41
Salim Djerbouh <github@caddydz.me>
2018-06-25 13:28:01 +0100

    Converting raw values to enum types
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,6 +15,18 @@ print("Center has raw value \(TextAlignment.center.rawValue)")
 print("Justify has raw value \(TextAlignment.justify.rawValue)")
 print("The alignment variable has raw value \(alignment.rawValue)")
 
+// Create a raw value
+let myRawValue = 20
+
+// Try to convert the raw value into a TextAlignment
+if let myAlignment = TextAlignment(rawValue: myRawValue) {
+    // Conversion succeeded!
+    print("successfully converted \(myRawValue) into a TextAlignment")
+} else {
+    // Conversion failed
+    print("\(myRawValue) has no corresponding TextAlignment case")
+}
+
 switch alignment {
 case .left:
     print("left aligned")
