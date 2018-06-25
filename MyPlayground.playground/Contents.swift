74a583244a79849df3bd88c0e8b4292d1419137a
Salim Djerbouh <github@caddydz.me>
2018-06-25 13:24:05 +0100

    Confirming the raw values
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,6 +9,12 @@ enum TextAlignment: Int {
 
 var alignment = TextAlignment.justify
 
+print("Left has raw value \(TextAlignment.left.rawValue)")
+print("Right has raw value \(TextAlignment.right.rawValue)")
+print("Center has raw value \(TextAlignment.center.rawValue)")
+print("Justify has raw value \(TextAlignment.justify.rawValue)")
+print("The alignment variable has raw value \(alignment.rawValue)")
+
 switch alignment {
 case .left:
     print("left aligned")
