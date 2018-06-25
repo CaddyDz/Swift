4f440bf66a6665bcc0ce5bbf28352889e855f03b
Salim Djerbouh <github@caddydz.me>
2018-06-25 09:48:39 +0100

    Closure Expression Syntax
    Inline closure as the function's final argument
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,4 @@ import Cocoa
 
 let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
-let volunteersSorted = vollunteerCounts.sorted(by: {$0 < $1})
+let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
