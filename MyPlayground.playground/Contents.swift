e55ca72d55c5ba832ff8026f451e259b4323dab6
Salim Djerbouh <github@caddydz.me>
2018-06-25 11:26:51 +0100

    Functions as Arguments (fixes)
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,9 +4,9 @@ let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
 let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
 
-func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int) {
+func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int)? {
     if condition(budget) {
-        func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int? {
+        func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int {
             return lights + existingLights
         }
         return buildRoads
@@ -16,13 +16,10 @@ func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, I
 }
 
 func evaluate(budget: Int) -> Bool {
-    return budget > 10_000
+    return budget > 10000
 }
 var stopLights = 4
-if let townPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 1000, condition: true) {
-    stopLights = townPlanByAddingLightsToExistingLights(4, stopLights)
-}
-if let newTownPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 10_500, condition: evaluate) {
+if let newTownPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 10500, condition: evaluate) {
     stopLights = newTownPlanByAddingLightsToExistingLights(4, stopLights)
 }
 print("Knowhere has \(stopLights) stoplights.")
