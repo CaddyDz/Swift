6ae28dc516bef80adcd6305354a963a72e8bd7f8
Salim Djerbouh <github@caddydz.me>
2018-06-25 10:50:04 +0100

    Functions as Arguments
    Building more roads
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
 let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
 
-func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int?) {
+func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int) {
     if condition(budget) {
         func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int? {
             return lights + existingLights
@@ -21,5 +21,8 @@ func evaluate(budget: Int) -> Bool {
 var stopLights = 4
 if let townPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 1000, condition: true) {
     stopLights = townPlanByAddingLightsToExistingLights(4, stopLights)
+}
+if let newTownPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 10_500, condition: evaluate) {
+    stopLights = newTownPlanByAddingLightsToExistingLights(4, stopLights)
 }
 print("Knowhere has \(stopLights) stoplights.")
