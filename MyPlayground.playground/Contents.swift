bcb431f6ddfae9b28ae90735096d1f043b20ce16
Salim Djerbouh <github@caddydz.me>
2018-06-25 09:43:23 +0100

    Closure Expression Syntax
    Taking advantage of type inference
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,4 @@ import Cocoa
 
 let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
-let volunteersSorted = vollunteerCounts.sorted(by: {
-    (i: Int, j: Int) -> Bool in
-    return i < j
-})
+let volunteersSorted = vollunteerCounts.sorted(by: {i,j in i < j})
