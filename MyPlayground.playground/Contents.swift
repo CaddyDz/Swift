74b98c7479bc92dee3e36c45fb472fdba46f6bea
Salim Djerbouh <github@caddydz.me>
2018-06-25 10:46:39 +0100

    Functions as Arguments
    Adding budget considerations
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,14 +4,22 @@ let vollunteerCounts = [1,3,40,32,2,53,77,13]
 
 let volunteersSorted = vollunteerCounts.sorted{$0 < $1}
 
-func makeTownGrand() -> (Int, Int) -> Int {
-    func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int {
-        return lights + existingLights
+func makeTownGrand(withBudget budget: Int, condition: (Int) -> Bool) -> ((Int, Int) -> Int?) {
+    if condition(budget) {
+        func buildRoads(byAddingLights lights: Int, toExistingLights existingLights: Int) -> Int? {
+            return lights + existingLights
+        }
+        return buildRoads
+    } else {
+        return nil
     }
-    return buildRoads
 }
 
+func evaluate(budget: Int) -> Bool {
+    return budget > 10_000
+}
 var stopLights = 4
-let townPlanByAddingLightsToExistingLights = makeTownGrand()
-stopLights = townPlanByAddingLightsToExistingLights(4, stopLights)
+if let townPlanByAddingLightsToExistingLights = makeTownGrand(withBudget: 1000, condition: true) {
+    stopLights = townPlanByAddingLightsToExistingLights(4, stopLights)
+}
 print("Knowhere has \(stopLights) stoplights.")
