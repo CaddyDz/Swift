ef9bdffd34c1ccba08ce4cd8cee0fbdbcd132f01
Salim Djerbouh <caddydz4@gmail.com>
2020-12-14 01:51:38 +0100

    Added code from derek banas
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,10 @@
 import Cocoa
 
 enum TextAlignment: Int {
-    case left = 20
-    case right = 30
-    case center = 40
-    case justify = 50
+	case left = 20
+	case right = 30
+	case center = 40
+	case justify = 50
 }
 
 var alignment = TextAlignment.justify
@@ -20,54 +20,54 @@ let myRawValue = 100
 
 // Try to convert the raw value into a TextAlignment
 if let myAlignment = TextAlignment(rawValue: myRawValue) {
-    // Conversion succeeded!
-    print("successfully converted \(myRawValue) into a TextAlignment")
+	// Conversion succeeded!
+	print("successfully converted \(myRawValue) into a TextAlignment")
 } else {
-    // Conversion failed
-    print("\(myRawValue) has no corresponding TextAlignment case")
+	// Conversion failed
+	print("\(myRawValue) has no corresponding TextAlignment case")
 }
 
 switch alignment {
 case .left:
-    print("left aligned")
+	print("left aligned")
 case .right:
-    print("right aligned")
+	print("right aligned")
 case .center:
-    print("center aligned")
+	print("center aligned")
 case .justify:
-    print("justified")
+	print("justified")
 }
 
 enum ProgrammingLanguage: String {
-    case swift
-    case objectiveC = "objective-c"
-    case c
-    case cpp = "c++"
-    case java = "java"
+	case swift
+	case objectiveC = "objective-c"
+	case c
+	case cpp = "c++"
+	case java = "java"
 }
 
 let myFavoriteLanguage = ProgrammingLanguage.c
 print("My favorite programming language is \(myFavoriteLanguage)")
 
 enum Lightbulb {
-    case on
-    case off
-    func sufraceTemperature(forAmbientTemperature ambient: Double) -> Double {
-        switch self {
-        case .on:
-            return ambient + 150.0
-        case .off:
-            return ambient
-        }
-    }
-    mutating func toggle() {
-        switch self {
-        case .on:
-            self = .off
-        case .off:
-            self = .on
-        }
-    }
+	case on
+	case off
+	func sufraceTemperature(forAmbientTemperature ambient: Double) -> Double {
+		switch self {
+		case .on:
+			return ambient + 150.0
+		case .off:
+			return ambient
+		}
+	}
+	mutating func toggle() {
+		switch self {
+		case .on:
+			self = .off
+		case .off:
+			self = .on
+		}
+	}
 }
 
 var bulb = Lightbulb.on
@@ -80,24 +80,24 @@ bulbTemperature = bulb.sufraceTemperature(forAmbientTemperature: ambientTemperat
 print("the bulb's temperature is \(bulbTemperature)")
 
 enum ShapeDimensions {
-    // point has no associated value - it is dimensionless
-    case point
-    // square's associated value is the length of one side
-    case square(side: Double)
-    
-    // rectangle's associated value defines its width and height
-    case rectangle(width: Double, height: Double)
-    
-    func area() -> Double {
-        switch self {
-        case .point:
-            return 0
-        case let .square(side: side):
-            return side * side
-        case let .rectangle(width: w, height: h):
-            return w * h
-        }
-    }
+	// point has no associated value - it is dimensionless
+	case point
+	// square's associated value is the length of one side
+	case square(side: Double)
+
+	// rectangle's associated value defines its width and height
+	case rectangle(width: Double, height: Double)
+
+	func area() -> Double {
+		switch self {
+		case .point:
+			return 0
+		case let .square(side: side):
+			return side * side
+		case let .rectangle(width: w, height: h):
+			return w * h
+		}
+	}
 }
 
 var squareShape = ShapeDimensions.square(side: 10.0)
@@ -108,9 +108,9 @@ print("rectangle's area = \(rectShape.area())")
 print("point's area = \(pointShape.area())")
 
 enum FamilyTree {
-    case noKnownParents
-    indirect case oneKnownParent(name: String, ancestor: FamilyTree)
-    indirect case twoKnownParents(fatherName: String, fatherAncestors: FamilyTree, motherName: String, motherAncestor: FamilyTree)
+	case noKnownParents
+	indirect case oneKnownParent(name: String, ancestor: FamilyTree)
+	indirect case twoKnownParents(fatherName: String, fatherAncestors: FamilyTree, motherName: String, motherAncestor: FamilyTree)
 }
 
 let fredAncestors = FamilyTree.twoKnownParents(fatherName: "Fred Sr.", fatherAncestors: .oneKnownParent(name: "Beth", ancestor: .noKnownParents), motherName: "Marsha", motherAncestor: .noKnownParents)
